7744785d7c24332141d17774578cfe6b813a04fe
yeechianlow <45187466+yeechianlow@users.noreply.github.com>
2019-07-23 16:19:03 -0400

    Change pickle protocol for Python 2 compatibility

--- mosx/model/predictors.py
+++ mosx/model/predictors.py
@@ -87,7 +87,7 @@ def format_predictors(config, bufr_file, obs_file, verif_file, output_file=None,
         output_file = '%s/%s_predictors.pkl' % (config['site_directory'], config['station_id'])
     print('predictors: -> exporting to %s' % output_file)
     with open(output_file, 'wb') as handle:
-        pickle.dump(export_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(export_dict, handle, protocol=2)
 
     if return_dates:
         return all_dates
